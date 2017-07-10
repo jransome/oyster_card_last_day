39e910ff5a35d54ba9a60d43edff260bd790abc3
annalaise <annalaise.gibbons@gmail.com>
2017-07-10 15:57:44 +0100

    unit test for creating card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,14 @@
+require './lib/oystercard.rb'
+
 describe Oystercard do
-end 
+  subject(:card) { described_class.new }
+
+  it 'creates card' do
+   expect(card).to respond_to(:balance)
+  end
+
+  it 'has initial balance of 0' do
+    expect(card).to have_attributes(:balance => 0)
+  end
+
+end
