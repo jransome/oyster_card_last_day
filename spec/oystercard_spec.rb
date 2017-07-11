d2fec65b31a0d2fb56277669ab34b3523c9256a0
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-11 13:23:33 +0100

    progress on challenge 9

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,35 +2,37 @@ require './lib/oystercard.rb'
 
 describe Oystercard do
   subject(:card) { described_class.new }
+  let(:top_up) { 50 }
+  let(:max_balance) { described_class::MAX_BALANCE }
+  let(:minumum_balance) { 1 }
 
   it 'creates card' do
    expect(card).to respond_to(:balance)
   end
 
   it 'has initial balance of 0' do
-    expect(card).to have_attributes(:balance => 0)
+    expect(card).to have_attributes(balance: 0)
   end
 
-  describe '#top_up' do
+  context '#top_up' do
     it 'increments the card balance' do
-      expect { card.top_up(1) }.to change { card.balance }.by 1
+      expect { card.top_up(top_up) }.to change { card.balance }.by top_up
     end
 
     it 'enforces max balance of £90' do
-      max_balance = Oystercard::MAX_BALANCE
       expect { card.top_up(max_balance + 1) }.to raise_error ("Top up limit is £#{max_balance}")
     end
   end
 
-  describe '#deduct' do
+  context '#deduct' do
     it 'deducts an amount from the balance' do
-      subject.top_up(20)
+      subject.top_up(top_up)
       expect { card.deduct(1) }.to change { card.balance }.by -1
     end
   end
 
 context 'requires top-up to travel' do
-  before { card.top_up(Oystercard::MAX_BALANCE) }
+  before { card.top_up(max_balance) }
 
     describe '#in_journey?' do
       it 'is initially not in a journey' do
@@ -50,6 +52,14 @@ context 'requires top-up to travel' do
         card.touch_out
         expect(card).not_to be_in_journey
       end
+    end
+
+    it "requires a minimum balance on a card to start a journey" do
+      expect(card.balance).to satisfy { |balance| balance >= minumum_balance }
+    end
+
+    it "raises an error if there is not enough funds for the minimum fare" do
+      
     end
   end
 end
